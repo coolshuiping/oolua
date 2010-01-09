5b4fcfc7968fdc23c042378dc4946314f1e5634c
liamdevine <liamdevine@078c8882-b850-11de-b71e-ab0a53e69959>
2010-01-09 14:04:54 +0000

    * fixed typo in xcode_premake4.sh

--- branches/premake4/xcode_premake4.sh
+++ branches/premake4/xcode_premake4.sh
@@ -10,7 +10,7 @@ cd unit_tests
 xcodebuild  -configuration Debug | tee ../build_logs/xcode_debug.log
 xcodebuild  -configuration Release | tee ../build_logs/xcode_release.log
 cd ../tests_may_fail
-xcodebuild  -configuration Debugl | tee ../build_logs/may_fail_gnu_debug.log
+xcodebuild  -configuration Debug | tee ../build_logs/may_fail_gnu_debug.log
 xcodebuild  -configuration Release | tee ../build_logs/may_fail_gnu_release.log
 cd ..
 premake4 clean
