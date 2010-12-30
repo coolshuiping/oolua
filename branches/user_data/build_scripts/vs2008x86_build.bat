5f82a6daec660cdfb2990dee2a5150aa2d2285e5
liamdevine <liamdevine@078c8882-b850-11de-b71e-ab0a53e69959>
2010-12-30 21:58:53 +0000

    Added a missing path separator in the vs2008 build scipts

--- branches/user_data/build_scripts/vs2008x86_build.bat
+++ branches/user_data/build_scripts/vs2008x86_build.bat
@@ -15,11 +15,11 @@ md local_install\oolua
 :: start devenv
 CALL "C:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
 
-devenv  "%cd%\oolua.sln" /build debug /project "%cd%oolua.vcproj" > .\build_logs\oolua_vs2008_debug.log
-devenv  "%cd%\oolua.sln" /build release /project "%cd%oolua.vcproj" > .\build_logs\oolua_vs2008_release.log
+devenv  "%cd%\oolua.sln" /build debug /project "%cd%\oolua.vcproj" > .\build_logs\oolua_vs2008_debug.log
+devenv  "%cd%\oolua.sln" /build release /project "%cd%\oolua.vcproj" > .\build_logs\oolua_vs2008_release.log
 
 copy .\include\*.h .\local_install\oolua\*.h 
 copy .\bin\Debug\*.lib .\local_install\
 copy .\bin\Release\*.lib .\local_install\
 
-premake4.exe clean 
+premake4.exe clean
